9fd5d3ad2e6134362d1dd0311458eb42b69dcbae
Fredrik Tõnisson-Bystam <fredrik.bystam@hedvig.com>
2023-08-30 11:27:13 +0200

    AstGenerator

---
+++ jlox/src/main/kotlin/tool/GenerateAst.kt
@@ -0,0 +1,88 @@
+package tool
+
+import java.io.PrintWriter
+
+
+object GenerateAst {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val projectDirectory = System.getProperty("user.dir")
+        val outputDir = "$projectDirectory/src/main/kotlin/lox"
+
+        defineAst(outputDir, "Expr", listOf(
+            "Binary   : Expr left, Token operator, Expr right",
+            "Grouping : Expr expression",
+            "Literal  : Any value",
+            "Unary    : Token operator, Expr right"
+        ));
+    }
+
+    private fun defineAst(
+        outputDir: String,
+        baseName: String,
+        types: List<String>
+    ) {
+        val path = "$outputDir/$baseName.kt"
+        val writer = PrintWriter(path, "UTF-8")
+        writer.println("package lox")
+        writer.println()
+        writer.println()
+        writer.println("sealed interface $baseName {")
+
+        writer.println()
+        defineVisitor(writer, baseName, types)
+        writer.println()
+
+        writer.println("    fun <R> accept(visitor: Visitor<R>): R")
+        writer.println()
+
+        for (type in types) {
+            val pieces = type.split(":").map { piece -> piece.trim() }
+
+            val (className, fields) = pieces
+            defineType(writer, baseName, className, fields)
+        }
+        writer.println("}")
+        writer.close()
+    }
+
+    private fun defineType(
+        writer: PrintWriter,
+        baseName: String,
+        className: String,
+        fieldList: String
+    ) {
+        writer.println(
+            "    data class $className("
+        )
+
+        // Store parameters in fields.
+        val fields = fieldList.split(", ")
+        for (field in fields) {
+            val (type, name) = field.split(" ")
+            writer.println("        val $name: $type,")
+        }
+        writer.println("    ) : $baseName {")
+        writer.println("""
+            override fun <R> accept(visitor: Visitor<R>): R = visitor.visit$className$baseName(this)
+        """.trimIndent().prependIndent("        "))
+        writer.println("    }")
+        writer.println()
+    }
+
+    private fun defineVisitor(
+        writer: PrintWriter,
+        baseName: String,
+        types: List<String>
+    ) {
+        writer.println("    interface Visitor<R> {")
+
+        for (type in types) {
+            val typeName = type.split(":").first().trim()
+            writer.println("        fun visit$typeName$baseName(${baseName.lowercase()}: $typeName): R")
+        }
+
+        writer.println("    }")
+    }
+}
